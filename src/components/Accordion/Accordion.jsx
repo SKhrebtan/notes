30dd9f161d1ca75e359eaa2a129a92896cc3610f
Mac <shrebtan@gmail.com>
2023-12-07 12:00:41 +0200

    enough for first time

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,14 +1,31 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { AccordionData } from "./data";
 import { AccordionItem } from "./AccordionItem";
 import './Accordion.css';
 
 export const Accordion = () => {
     const [openId, setOpenId] = useState(null);
+    const [filterValue, setFilterValue] = useState('');
+    const changeFilter = (event) => setFilterValue(event.currentTarget.value);
+    const normalizedFilter = filterValue.toLowerCase();
+    const filteredTerms = useMemo(() => {
+        if (AccordionData) { 
+        return AccordionData.filter(item =>
+          item.q.toLowerCase().includes(normalizedFilter))
+      }
+    },[normalizedFilter])
 
     return (
+        <>
+        <input 
+        type="text"
+         name="filter"
+         className="input"
+         value={filterValue}
+         onChange={changeFilter}
+         />
         <ul className="accordion">
-            {AccordionData.map((faqItem, id) => 
+            {filteredTerms.map((faqItem, id) => 
                 <AccordionItem
                     onClick={()=> id===openId ? setOpenId(null) : setOpenId(id)}
                     faqItem={faqItem}
@@ -17,5 +34,6 @@ export const Accordion = () => {
                 />
             )}
         </ul>
+</>
     )
 }
---
+++ src/components/Accordion/data.js
@@ -0,0 +1,112 @@
+export const AccordionData = [
+    {
+        id: '01',
+        q: 'GraphQL',
+        a: 'GraphQL is a query language for your API, defining types and fields on those types, not tied to any specific database, backed by existing code.'
+    },
+{
+    id: '02',
+     q: 'NextJs',
+        a: "Next is a React framework that enables several extra features, including server-side rendering and generating static websites, automatic code-splitting for faster page loads. Simple client-side routing. Able to implement with express or any other NodeJs server. "
+    },
+    {
+        id: '03',
+         q: 'React',
+        a: 'React is an open-source frontend JS library that used for building composable user interface, especially for single page-application. It uses JSX syntax, that allows developers to write HTML in JS code. It uses virtual DOM insteat of Real considering that Real DOM manipulations are expensive. Follows undirectional or one-way data flow or data bindings. Uses reusable / composable UI components to develop the view.'
+    },
+    {
+        id: '04',
+         q: 'Typescript',
+        a: 'TypeScript is a programming language developed and maintained by Microsoft. It is a superset of JavaScript, meaning that all valid JavaScript code is also valid TypeScript code. TypeScript adds static typing to JavaScript, which is not inherently present in the standard JavaScript language. Use of interfaces for complex type definitions. Avoid bugs with the ease of readability and validation.'
+    },
+    {
+        id: '05',
+         q: 'Library / Framework',
+        a: 'The key distinction lies in the level of control and structure provided. A library is a collection of functions or classes taht can be used by an application, allowing developers to retain control over the applications structure. In contrast, a framework provides a higher-level structure, dictates the flow of control, and often requires developers to conform to its conventions for building applications.'
+    },
+    {
+        id: '06',
+         q: 'REST API',
+        a: 'An API, or application programming interface, is a set of rules that define how applications or devices can connect to and communicate with each other. A REST API is an API that conforms to the design principles of the REST, or representational state transfer architectural style.'
+    },
+    {
+        id: '07',
+         q: 'AI Integration',
+        a: 'AI integration refers to the process of incorporating artificial intelligence (AI) technologies and capabilities into existing systems, applications, or processes. The goal is to enhance or automate certain functionalities, enabling better decision-making, improved efficiency, and innovative features. AI integration can occur in various domains, including software development, business operations, healthcare, finance, and more.'
+    },
+    {
+        id: '08',
+         q: 'MySQL',
+        a: 'MySQL is an open-source relational database management system (RDBMS) that is widely used for managing and organizing data. It is a popular choice for web applications and is a key component of the LAMP (Linux, Apache, MySQL, PHP/Python/Perl) stack, which is a common software stack for building and deploying dynamic websites.'
+    },
+    {
+        id: '09',
+         q: 'MongoDB',
+        a: 'MongoDB is a popular open-source NoSQL database management system that is designed to store and retrieve data in a flexible, scalable, and JSON-like format. Unlike traditional relational database management systems (RDBMS), MongoDB is a document-oriented database, belonging to the family of NoSQL databases.'
+    },
+    {
+        id: '10',
+         q: 'PostgreSQL',
+        a: 'PostgreSQL, often referred to as "Postgres," is a powerful and open-source relational database management system (RDBMS). It is known for its advanced features, extensibility, and compliance with SQL standards. PostgreSQL is designed to handle complex workloads, support concurrent transactions, and provide robust data integrity.'
+    },
+    {
+        id: '11',
+         q: 'Jira / Trello',
+        a: 'Jira and Trello are both popular project management tools, but they have different features, use cases, and target audiences. JIRA is designed to help teams plan, track, and manage their work efficiently. Originally developed for software development teams, JIRA has become popular across various industries and teams for managing projects and tasks. TRELLO provides a flexible and easy-to-use interface, making it suitable for a variety of projects and collaboration needs. Trello is known for its simplicity and is widely used for both professional and personal task management.'
+    },
+    {
+        id: '12',
+         q: 'Bitbucket',
+        a: 'Bitbucket is a web-based platform for version control and collaboration. It is primarily used for hosting Git repositories, but it also supports Mercurial repositories. Bitbucket is developed and maintained by Atlassian, the same company behind popular tools like Jira and Confluence.'
+    },
+    {
+        id: '13',
+         q: 'Git',
+        a: 'Git is a distributed version control system used for tracking changes in source code during software development. It was created by Linus Torvalds in 2005 and has become the de facto standard for version control in the software development industry. Git is designed to be fast, flexible, and efficient, making it suitable for both small and large projects.'
+    },
+    {
+        id: '14',
+         q: 'Github',
+        a: 'GitHub is a web-based platform that uses Git for version control and provides a collaborative environment for software development projects. It serves as a hosting service for Git repositories and offers a range of features to facilitate collaboration, code review, and project management. GitHub is widely used by developers, teams, and organizations for sharing, collaborating on, and contributing to open-source and private projects.'
+    },
+    {
+        id: '15',
+         q: 'Docker',
+        a: 'Docker is a platform and toolset for developing, shipping, and running applications in containers. Containers are lightweight, portable, and self-sufficient units that encapsulate software and its dependencies, ensuring consistent and reliable execution across different environments. Docker simplifies the process of building, deploying, and scaling applications by providing a standardized way to package, distribute, and run them.'
+    },
+    {
+        id: '16',
+         q: 'Postman',
+        a: 'Postman is a popular collaboration platform for API development. It provides a user-friendly interface for sending HTTP requests, testing APIs, and managing the entire API development lifecycle. Postman simplifies the process of working with APIs, allowing developers to design, develop, and test APIs more efficiently.'
+    },
+    {
+        id: '17',
+         q: 'Figma',
+        a: 'Figma is a cloud-based design and prototyping tool that facilitates collaboration among designers, developers, and other stakeholders in the product development process. It is widely used for creating user interfaces (UIs), user experiences (UX), and interactive prototypes for web and mobile applications. Figmas key strength lies in its real-time collaboration features, which allow multiple users to work on a design project simultaneously.'
+    },
+    {
+        id: '18',
+         q: 'Adobe XD',
+        a: 'Adobe XD (Experience Design) is a vector-based design and prototyping tool developed by Adobe Inc. It is specifically geared towards UI/UX design for web and mobile applications. Adobe XD enables designers to create interactive prototypes, design interfaces, and collaborate with stakeholders throughout the design and development process.'
+    },
+    {
+        id: '18',
+         q: 'GCP',
+        a: 'Google Cloud Platform (GCP) is a suite of cloud computing services provided by Google. GCP offers a wide range of services for computing, storage, data analytics, machine learning, networking, and more. It is designed to provide scalable and flexible cloud infrastructure to support the development, deployment, and management of applications and services.'
+    },
+    {
+        id: '19',
+         q: 'AWS',
+        a: 'Amazon Web Services (AWS) is a comprehensive and widely-used cloud computing platform provided by Amazon. AWS offers a vast array of cloud services, including computing power, storage, databases, machine learning, analytics, security, and more. It is designed to provide scalable and reliable cloud infrastructure for businesses and developers.'
+    },
+    {
+        id: '20',
+         q: 'Imperative / Declarative Programming',
+        a: 'In Imperative programming, we have to specify each step of building anything, such as a user interface. Whereas in Declarative programming, we just need to describe the end product, and the software will create it for us. It takes comparatively less effort and time.'
+    },
+    {
+        id: '21',
+         q: 'SEO',
+        a: 'SEO, or Search Engine Optimization, is a set of practices and strategies aimed at optimizing a website to improve its visibility and ranking in search engine results pages (SERPs). The ultimate goal of SEO is to drive organic (non-paid) traffic to a website, increase its visibility, and enhance its online presence.'
+    }
+]
